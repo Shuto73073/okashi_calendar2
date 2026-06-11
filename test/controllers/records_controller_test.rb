8adfd53d4f3cbab1690660771733197c3a33fc43
shuto <akiyama73073@gmail.com>
2026-06-11 13:51:32 +0900

    Update:test

--- test/controllers/records_controller_test.rb
+++ test/controllers/records_controller_test.rb
@@ -1,23 +1,39 @@
 require "test_helper"
 
 class RecordsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    # ユーザーを作成
+    @user = User.create!(name: "Test User", password: "password")
+    
+    # ログイン処理
+    post login_url, params: { name: @user.name, password: "password" }
+    
+    # テスト用のレコードを作成
+    @record = Record.create!(
+      date: Time.current,           
+      snack_name: "Test Snack",     
+      amount: Record::AMOUNTS[:medium],
+      user: @user                   
+    )
+  end
+
   test "should get index" do
-    get records_index_url
+    get records_path
     assert_response :success
   end
 
   test "should get show" do
-    get records_show_url
+    get record_path(@record)
     assert_response :success
   end
 
   test "should get new" do
-    get records_new_url
+    get new_record_path
     assert_response :success
   end
 
   test "should get edit" do
-    get records_edit_url
+    get edit_record_path(@record)
     assert_response :success
   end
 end
