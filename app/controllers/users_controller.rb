53091fff07cb312a635d48d4b5bf927c117c9a6d
shuto <akiyama73073@gmail.com>
2026-06-13 12:25:40 +0900

    users_controller update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to records_path, notice: "ユーザー登録が完了しました"
+      redirect_to calendar_records_path, notice: "ユーザー登録が完了しました"
     else
       render :new, status: :unprocessable_entity
     end
